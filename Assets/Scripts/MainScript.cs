0be27b26fda1bd5e5402d640018c73bd046cd501
Daniel Abk <danikabk@gmail.com>
2023-01-08 19:27:37 -0800

    Failed attempt at making a moveable player using arrow tiles

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -8,6 +8,9 @@ public class MainScript : MonoBehaviour
     public GameObject tilePrefab;
     public GameObject backgroundImage;
     public GameObject canvas;
+    Maze maze1;
+    Vector2 playerPos;
+    Vector2[] adjacent = new Vector2[4];
     int tileSizePX = 30;
     // Start is called before the first frame update
     [System.Serializable]
@@ -18,7 +21,7 @@ public class MainScript : MonoBehaviour
     }
     void Start()
     {
-        Maze maze1 = GetMazeFromJSON();
+        maze1 = GetMazeFromJSON();
         int[,] mazeArray = Get2DArrayFromMaze(maze1);
         GenerateMaze(maze1, mazeArray);
         SetupButtonListeners();
@@ -52,16 +55,25 @@ public class MainScript : MonoBehaviour
         cloneTile.name = index.x + "," + index.y;
         if (code == 0)
         {
+            // Code 0 is a Walking Path
             tileImage.color = new Color(255f, 255f, 255f, 1f);
             //tileButton.colors.normalColor = new Color();
         } else if (code == 1)
         {
+            // Code 1 is a Wall
             tileImage.color = new Color(0f, 0f, 0f, 1f);
+            //Rigidbody2D rigidBody = cloneTile.AddComponent<Rigidbody2D>();
+            //BoxCollider2D boxCollider = cloneTile.AddComponent<BoxCollider2D>();
+            //ConstantForce2D force2D = cloneTile.AddComponent<ConstantForce2D>();
+            //force2D.relativeForce = new Vector2(1, 0);
         } else if (code == 2)
         {
+            // Code 2 is the Start
             tileImage.color = new Color(0f, 255f, 0f, 1f);
+            playerPos = pos;
         } else if (code == 3)
         {
+            // Code 3 is the Finish
             tileImage.color = new Color(255f, 0f, 0f, 1f);
         }
     }
@@ -93,10 +105,33 @@ public class MainScript : MonoBehaviour
         Debug.Log(button.transform.name);
     }
 
+    void CheckAdjacent()
+    {
+        adjacent[0] = new Vector2(playerPos.x - 1, playerPos.y);
+        adjacent[1] = new Vector2(playerPos.x, playerPos.y + 1);
+        adjacent[2] = new Vector2(playerPos.x + 1, playerPos.y);
+        adjacent[3] = new Vector2(playerPos.x, playerPos.y - 1);
+    }
+
+    Transform TileFromPosition(Vector2 pos)
+    {
+        //return maze1.maze[pos.x * 10 + pos.y];
+        if (pos.x < 0 || pos.x > 9 || pos.y < 0 || pos.y > 9)
+            return null;
+        return backgroundImage.transform.Find((pos.x).ToString() + "," + (pos.y).ToString());
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckAdjacent();
+        for(int x = 0; x < 4; x++)
+        {
+            var adjacentTile = TileFromPosition(adjacent[x]);
+            if (adjacentTile == null)
+                continue;
+            Image tileImage = adjacentTile.parent.GetComponent<Image>();
+            tileImage.color = new Color(220f, 220f, 220f, 1f);
+        }
     }
 }
